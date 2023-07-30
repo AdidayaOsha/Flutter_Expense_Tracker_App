8c44c18c23c3c1cb96909b6bd4a2b0edb961627c
AdidayaOsha <96316005+AdidayaOsha@users.noreply.github.com>
2023-07-30 13:01:37 +0700

    added dark theme to main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 // k is a conventional naming for Global Variable
-var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  // telling flutter that this color scheme is for the darkmode
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
+      // DARK THEME
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+
+      //LIGHT THEME
       theme: ThemeData().copyWith(
         useMaterial3: true,
         // Define one color Scheme
@@ -20,7 +47,10 @@ void main() {
         // CARD THEME
         cardTheme: const CardTheme().copyWith(
           color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
         ),
         // ELEVATED BUTTON THEME
         elevatedButtonTheme: ElevatedButtonThemeData(
@@ -36,6 +66,7 @@ void main() {
                   fontSize: 17),
             ),
       ),
+      themeMode: ThemeMode.system, //default
       home: const Expenses(),
     ),
   );
